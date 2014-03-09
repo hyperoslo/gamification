d7d46299d4f91e7f65011c5b8d31c46ba826d51a
Johannes Gorset <jgorset@gmail.com>
2014-03-10 00:56:45 +0100

    Fix a bug that caused the scorings path to be wrong if used inside another engine

--- app/helpers/gamification/application_helper.rb
+++ app/helpers/gamification/application_helper.rb
@@ -1,11 +1,9 @@
 module Gamification
   module ApplicationHelper
-    include Gamification::Engine.routes.url_helpers
-
     def complete taskable, options
       subjectable = options[:for]
 
-      form_tag scorings_path, method: :post do
+      form_tag Gamification::Engine.routes.url_helpers.scorings_path, method: :post do
         concat hidden_field_tag 'scoring[taskable_type]', taskable.class.name
         concat hidden_field_tag 'scoring[taskable_id]', taskable.id
         concat hidden_field_tag 'scoring[subjectable_type]', subjectable.class.name

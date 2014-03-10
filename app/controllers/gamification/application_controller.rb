5bb01fb9ca183865d20e0d67fbbb7cc3e09fe41e
Johannes Gorset <jgorset@gmail.com>
2014-03-10 16:37:58 +0100

    Fix layouts

--- app/controllers/gamification/application_controller.rb
+++ app/controllers/gamification/application_controller.rb
@@ -1,4 +1,4 @@
 module Gamification
-  class ApplicationController < ActionController::Base
+  class ApplicationController < ::ApplicationController
   end
 end
---
+++ config/initializers/routing.rb
@@ -0,0 +1,15 @@
+module KnowledgeBase
+  module ApplicationHelper
+
+    # Knowledge base uses the layout of the client application, which causes Rails' url helpers to get confused
+    # and require you to prepend 'main_app' to your own routes. This hack allows developers to keep referring to
+    # routes without a prefix by sending undefined routes to 'main_app' before failing.
+    def method_missing(method, *args, &block)
+      if (method.to_s.end_with?('_path') || method.to_s.end_with?('_url')) && main_app.respond_to?(method)
+        main_app.send(method, *args)
+      else
+        super
+      end
+    end
+  end
+end

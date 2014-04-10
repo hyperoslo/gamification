bd56ae110a781bacf3d5ea9df61c80b8a10ded01
Johannes Gorset <jgorset@gmail.com>
2014-04-10 15:33:42 +0200

    Add with_medal trait to rewards factory

--- spec/factories/gamification_goals.rb
+++ spec/factories/gamification_goals.rb
@@ -4,5 +4,11 @@ FactoryGirl.define do
   factory :gamification_goal, aliases: [:goal], class: 'Gamification::Goal' do
     rewarding nil
     points 1
+
+    trait :with_medal do
+      after :create do |goal|
+        create :medal, goal: goal
+      end
+    end
   end
 end
